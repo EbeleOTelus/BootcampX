2ad63224516bf78acf5e6e870521573ec0cf18b2
Ebele <ebele.omanukwue@telus.com>
2022-12-14 22:51:43 -0700

    sql injection walkthrough

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,32 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+// pool.query(`
+// SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
+// FROM teachers
+// JOIN assistance_requests
+// ON assistance_requests.teacher_id = teachers.id
+// JOIN students
+// ON students.id = assistance_requests.student_id
+// JOIN cohorts
+// ON students.cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// GROUP BY cohorts.name, teachers.name
+// ORDER BY teachers.name;
+
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${process.argv[2]}:${row.teacher}`);
+//   })
+// });        
+
+
+const cohortsName = process.argv[2];
+
+const val = [`%${cohortsName}%`];
+
+const queryCommand = `
 SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
 FROM teachers
 JOIN assistance_requests
@@ -16,13 +41,15 @@ JOIN students
 ON students.id = assistance_requests.student_id
 JOIN cohorts
 ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY cohorts.name, teachers.name
 ORDER BY teachers.name;
 
-`)
+`;
+
+pool.query(queryCommand, val)
 .then(res => {
   res.rows.forEach(row => {
-    console.log(`${process.argv[2]}:${row.teacher}`);
+    console.log(`${row.cohort}:${row.teacher}`);
   })
-});        
+}).catch(err => console.error('query error', err.stack));
